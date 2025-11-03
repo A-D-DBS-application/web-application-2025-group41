f6d9ddb0871ba983af41522381c3d43059c171d7
UlrikeU41 <ulrike.uittenhove@ugent.be>
2025-11-03 11:06:45 +0100

    Update config.py

---
+++ flask_no_password_app/app/config.py
@@ -0,0 +1,4 @@
+class Config: 
+    SECRET_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mYmFiaGpuYXliam5qeGJmZGRyIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MDk0MjYzMSwiZXhwIjoyMDc2NTE4NjMxfQ.KHWrkBboIbumDVLR6e87cT1ErxgsOqU_oAPZRfBY_ps'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:[Group41!!!!]@db.mfbabhjnaybjnjxbfddr.supabase.co:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
