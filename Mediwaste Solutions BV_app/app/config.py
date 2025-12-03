eb0b7d487c8e8d20e5fe923e03d31a53f8554947
lowiekvdw <lowiek.vandewalle@ugent.be>
2025-12-03 14:33:26 +0100

    aanpassingen(niet af)

--- Mediwaste Solutions BV_app/app/config.py
+++ Mediwaste Solutions BV_app/app/config.py
@@ -10,7 +10,7 @@ class Config:
     # Connection string naar Supabase Postgres
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "SUPABASE_DB_URL",
-        "postgresql://postgres:[Group41!!!!]@db.mfbabhjnaybjnjxbfddr.supabase.co:5432/postgres"
+        "postgresql://postgres:Group41!!!!@db.mfbabhjnaybjnjxbfddr.supabase.co:5432/postgres"
     )
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
